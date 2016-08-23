68ef851c2350589491621cb42ab58569ec2d2fd0
César Loo <cesarlooe@gmail.com>
2016-08-23 21:45:39 +0000

    Actualización de plantillas
    Se actualizaron las plantillas 'home' y 'gig_detail' para utilizar datos dinámicos. Además se configuró el uso del MEDIA_URL.

--- pyverr/settings.py
+++ pyverr/settings.py
@@ -137,7 +137,24 @@ LOGIN_REDIRECT_URL = '/'
 SOCIAL_AUTH_FACEBOOK_KEY = '530655517131011'
 SOCIAL_AUTH_FACEBOOK_SECRET = '568fb51acb81c1d0b0354dc071cd7e02'
 
+SOCIAL_AUTH_PIPELINE = (
+    'social.pipeline.social_auth.social_details',
+    'social.pipeline.social_auth.social_uid',
+    'social.pipeline.social_auth.auth_allowed',
+    'social.pipeline.social_auth.social_user',
+    'social.pipeline.user.get_username',
+    'social.pipeline.user.create_user',
+    'pyverrapp.social_auth_pipeline.save_avatar',  # <--- set the path to the function
+    'social.pipeline.social_auth.associate_user',
+    'social.pipeline.social_auth.load_extra_data',
+    'social.pipeline.user.user_details',
+)
+
 # Replace database setting to use PostgreSQL on Heroku
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+# Setup upload directory for Gig model
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
